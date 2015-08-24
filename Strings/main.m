a58dc1db3ab6092d6a37cd6f597a04ff0a5248f0
Alp Eren Can <alperenc@gmail.com>
2015-08-24 15:41:20 -0700

    Initial Commit

---
+++ Strings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Strings
+//
+//  Created by Alp Eren Can on 24/08/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
