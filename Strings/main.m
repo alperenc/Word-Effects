bac5bf01fd45b59cb657d8d475f635ca474f3ac4
Alp Eren Can <alperenc@gmail.com>
2015-08-24 17:00:28 -0700

    user input, uppercase, lowercase, canadianize & respond

--- Strings/main.m
+++ Strings/main.m
@@ -10,8 +10,72 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        do {
+            // 255 unit long array of characters
+            char inputChars[255];
+            
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            // convert char array to an NSString object
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            
+            // print NSString object
+            NSLog(@"Input was: %@", inputString);
+            
+            char lastChar =[inputString characterAtIndex:inputString.length - 1];
+            
+            if (lastChar == '?') {
+                NSLog(@"I don't know.");
+            } else if (lastChar == '!') {
+                NSLog(@"Whoa, calm down!");
+            } else {
+                
+                printf("What do you want to do with this?\n");
+                printf("Choose an option:\n");
+                printf("1. Uppercase-it\n");
+                printf("2. Lowercase-it\n");
+                printf("3. Numberize-it\n");
+                printf("4. Canadianize-it\n");
+                printf("5. De-space-it\n");
+                
+                char operation = getchar();
+                NSLog(@"%c", operation);
+                
+                NSString *convertedString;
+                
+                switch (operation) {
+                    case '1':
+                        convertedString = [inputString uppercaseString];
+                        NSLog(@"%@",convertedString);
+                        break;
+                    case '2':
+                        convertedString = [inputString lowercaseString];
+                        NSLog(@"%@",convertedString);
+                        break;
+                    case '3':
+                        convertedString = [inputString lowercaseString];
+                        NSLog(@"%@",convertedString);
+                        break;
+                    case '4':
+                        convertedString = [inputString stringByAppendingString:@", eh?"];
+                        NSLog(@"%@",convertedString);
+                        break;
+                    case '5':
+                        convertedString = [inputString lowercaseString];
+                        NSLog(@"%@",convertedString);
+                        break;
+                    default:
+                        break;
+                }
+                
+                printf("You wanna do this again? (y/n)");
+            }
+        } while (false);
+        
     }
     return 0;
 }
